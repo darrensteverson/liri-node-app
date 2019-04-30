54c70c348e17a74ed2a6bed75bffd7af4c1da77a
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-04-30 14:54:09 -0400

    added variables, working on linking apis

--- liri.js
+++ liri.js
@@ -1,1 +1,8 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+var fs = require("fs");
+var axios = require("axios");
+var spotifyAPI = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
