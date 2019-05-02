a6d7c6f7a9ce54f85da160970efb905341a5da1d
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-05-02 17:09:17 -0400

    made switch cases started getband axios request

--- liri.js
+++ liri.js
@@ -1,8 +1,56 @@
 require("dotenv").config();
 
+
+// Variables for getting data from API's
 var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
-var spotifyAPI = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var liriCommand = process.argv[2];
+var userInput = process.argv.slice(3).join("");
 
+
+
+// Switch cases for each command
+function liriBot(liriCommand, userInput) {
+switch(liriCommand) {
+
+    case "movie-this":
+        getOMBD(userInput);
+        break;
+
+    case "concert-this":
+        getBands(userInput);
+        break;
+
+    case "spotify-this-song":
+        getSpotify(userInput);
+        break;
+
+    case "do-what-it-says":
+        getRandomTxt();
+        break;
+    }
+};
+
+function getbands(artistSearch){
+
+    var artistSearch = userInput;
+    var bandURL =  "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+
+    axios.get(bandURL).then(
+        function(response) {
+            console.log("Venue: " + response.data[0].venue.name + "\n");
+            console.log("City: " + response.data[0].venue.name + "\n");
+            console.log("Venue: " + moment(response.data[0].datetime).format("MM-DD-YYYY") + "\n");
+            console.log("_____________________________________________" + "\n");
+
+            fs.appendFile("log.txt", getbands, function (err) {
+                if (err) throw err;
+            });
+
+        });
+};
