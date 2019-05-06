f88bb96920da3e4809ab63b01cd62238478caeb9
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-05-06 10:33:58 -0400

    Can not find error, when running in command line, err shows

--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ function liriBot(liriCommand, userInput) {
 switch(liriCommand) {
 
     case "movie-this":
-        getOMBD(userInput);
+        getOMDB(userInput);
         break;
 
     case "concert-this":
@@ -35,9 +35,9 @@ switch(liriCommand) {
     }
 };
 // BandsInTown API
-function getBands(artistSearch){
+function getBands(){
 
-    var artistSearch = userInput;
+    var artist = userInput;
     var bandURL =  "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
 
@@ -58,23 +58,69 @@ function getBands(artistSearch){
 // Spotify API
 function getSpotify () {
 
-     if(songSearch = NULL){
+     if(!songSearch){
         songSearch = "The Sign";
-     } 
+     } else {
+         songSearch = userInput;
+     }
 };
-    spotify.search({ type: 'track', query: songSearch }, function(err, data) {
+    spotify.search({ type: 'track', query: userInput }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
+        } else {
+
+            var spotifyResults = 
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name + "\n")
+            console.log("Song: " + data.tracks.items[0].name + "\n")
+            console.log("Album: " + data.tracks.items[0].album.name + "\n")
+            console.log("_____________________________________________" + "\n")
         }
 
-        console.log("Artist: " + data.tracks.items[0].album.artists[0].name + "\n");
-        console.log("Song: " + data.tracks.items[0].name + "\n");
-        console.log("Album: " + data.tracks.items[0].album.name + "\n");
-        console.log("_____________________________________________" + "\n");
-
-        fs.appendFile("log.txt", getbands, function (err) {
+        fs.appendFile("log.txt", spotifyResults, function (err) {
             if (err) throw err;
         });
       });
+
+    //   OMDB API
+
+    function getOMDB () {
+        if (!movieSearch) {
+            movieSearch = "Mr.Nobody";
+        } else {
+            movieSearch = userInput;
+        }
+
+        var movieApiKey = "http://www.omdbapi.com/?t=" + movieSearch + "&y=short&apikey=trilogy";
+
+        axios.request(movieApiKey).then(
+            function(response) {
+                var movieResults =
+                    console.log("Title: " + response.data.title + "\n")
+                    console.log("Year: " + response.data.year + "\n")
+                    console.log("Rating: " + response.data.imbdRating + "\n")
+                    console.log("_____________________________________________" + "\n")
+            });
+
+            fs.appendFile("log.txt", movieResults, function (err) {
+                if (err) throw err;
+            });
+        };
+
+        // FS Random
+
+        function getRandomTxt() {
+            fs.append("random.text", "utf8", function (err, data) {
+                if(err) {
+                    throw err
+                } else {
+                    console.log (data);
+                }
+            });
+        };
+
+        liriBot(liriCommand, userInput);
+
+
+
 
 
